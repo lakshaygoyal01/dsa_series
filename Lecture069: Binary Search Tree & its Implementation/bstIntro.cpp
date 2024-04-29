3c768fe5bb8afee058dfd8ee4f31e40a5d6d39f5
Lakshay <lakshaygoyal020@gmail.com>
2024-04-29 20:46:45 +0530

    Binary Search Trees

---
+++ Lecture069: Binary Search Tree & its Implementation/bstIntro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class node{
+
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this-> data = d;
+        this-> left = NULL;
+        this->right = NULL;
+    }
+
+};
+
+int main(){
+
+    
+
+    return 0;
+}
